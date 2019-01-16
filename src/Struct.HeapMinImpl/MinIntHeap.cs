549d834daba984c09e4b0fa1114701c815d2f878
Vladimir Sundupey <vsundupey@gmail.com>
2019-01-16 23:00:52 +0300

    Completed implementation of heap, without testing

--- src/Struct.HeapMinImpl/MinIntHeap.cs
+++ src/Struct.HeapMinImpl/MinIntHeap.cs
@@ -31,5 +31,79 @@ namespace Struct.HeapMinImpl
         private int rightChild(int index) { return items[getLeftChildIndex(index)]; }
         private int parent(int index) { return items[getParentIndex(index)]; }
 
+        private void swap (int indexOne, int indexTwo)
+        {
+            int temp = items[indexOne];
+            items[indexOne] = items[indexTwo];
+            items[indexTwo] = temp;
+        }
+
+        private void ensureExtreCapacity()
+        {
+            if (size == capacity)
+            {
+                Array.Copy(items, items, capacity * 2);
+                capacity *= 2;
+            }
+        }
+
+        public int peek()
+        {
+            if (size == 0) throw new Exception("Heap doesn't contain elemants");  
+            return items[0];       
+        }
+
+        public int poll()
+        {
+            if (size == 0) throw new Exception("Heap doesn't contain elemants");  
+            int item = items[0];
+            items[0] = items[size - 1];
+            size--;
+            heapifyDown();
+            return item;
+        }
+
+        public void add(int item)
+        {
+            ensureExtreCapacity();
+            items[size] = item;
+            size++;
+            heapifyUp();
+        }
+
+        private void heapifyUp()
+        {
+            int index = size - 1;
+
+            while (hasParent(index) && parent(index) > items[index])
+            {
+                int pIndex = getParentIndex(index);
+                swap(pIndex, index);
+                index = pIndex;
+            }
+        }
+
+        private void heapifyDown()
+        {
+            int index = 0;
+
+            while (hasLeftChild(index))
+            {
+                int smallerIndex = getLeftChildIndex(index);
+
+                if (hasRightChild(index) && leftChild(index) > rightChild(index))
+                {
+                    smallerIndex = getRightChildIndex(index);
+                }
+
+                if (items[index] < items[smallerIndex])
+                {
+                    break;
+                }
+
+                swap(index, smallerIndex);
+                index = smallerIndex;
+            }
+        }
     }
 }
